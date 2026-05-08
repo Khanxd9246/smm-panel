70faa54e8016e7667d36c5b6585b73eae2c86481
Khanxd9246 <157797538+Khanxd9246@users.noreply.github.com>
2026-05-08 12:22:21 +0000

    FIX: Overwrote corrupted assets with valid multi-line source

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,5 @@
+import './bootstrap';
 import Alpine from 'alpinejs';
-import axios from 'axios';
 
 window.Alpine = Alpine;
-window.axios = axios;
-
 Alpine.start();
-
-// CSRF token setup for axios
-axios.defaults.headers.common['X-CSRF-TOKEN'] = document.querySelector('meta[name="csrf-token"]')?.getAttribute('content');
-
-console.log('SMM Elite panel ready!');
