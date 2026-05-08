33ca6f4c0159ce8d710735b5b8eba0feb64c9acf
Khanxd9246 <157797538+Khanxd9246@users.noreply.github.com>
2026-05-08 12:38:35 +0000

    Fix build issues and complete asset setup
    - Create missing resources/js/bootstrap.js with axios setup
    - Simplify app.js to standard Laravel structure
    - Fix corrupted tailwind.config.js with literal \n characters
    - Build now succeeds locally with proper asset compilation

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,14 +1,5 @@
 import './bootstrap';
 import Alpine from 'alpinejs';
-import axios from 'axios';
 
 window.Alpine = Alpine;
-window.axios = axios;
-
-// Set up X-CSRF-TOKEN header for axios
-const token = document.querySelector('meta[name="csrf-token"]');
-if (token) {
-    axios.defaults.headers.common['X-CSRF-TOKEN'] = token.getAttribute('content');
-}
-
 Alpine.start();
