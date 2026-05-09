ec603725790c25991e18aa6f55e3ad0cc46977f9
Khanxd9246 <157797538+Khanxd9246@users.noreply.github.com>
2026-05-09 09:40:41 +0500

    Update session.php

--- config/session.php
+++ config/session.php
@@ -1,10 +1,10 @@
-<?php
+?php
 use Illuminate\Support\Str;
 return [
-    'driver'          => env('SESSION_DRIVER', 'file'),
+    'driver'          => env('SESSION_DRIVER', 'redis'),
     'lifetime'        => env('SESSION_LIFETIME', 120),
     'expire_on_close' => false,
-    'encrypt'         => true,
+    'encrypt'         => false,
     'files'           => storage_path('framework/sessions'),
     'connection'      => env('SESSION_CONNECTION'),
     'table'           => 'sessions',
