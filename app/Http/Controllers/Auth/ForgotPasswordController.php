0ef53a53e59a58f3ff099ec76a4053f8181402e9
Khanxd9246 <157797538+Khanxd9246@users.noreply.github.com>
2026-05-09 02:09:57 -0700

    Update ForgotPasswordController.php

--- app/Http/Controllers/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Password;
 
 class ForgotPasswordController extends Controller
 {
@@ -34,49 +35,7 @@ class ForgotPasswordController extends Controller
             $this->credentials($request)
         );
 
-        return $response == \Password::RESET_LINK_SENT
-            ? back()->with('status', trans($response))
-            : back()->withInput($request->only('email'))
-                ->withErrors(['email' => trans($response)]);
-    }
-}
-<?php
-
-namespace App\Http\Controllers\Auth;
-
-use App\Http\Controllers\Controller;
-use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
-use Illuminate\Http\Request;
-
-class ForgotPasswordController extends Controller
-{
-    use SendsPasswordResetEmails;
-
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('guest');
-    }
-
-    /**
-     * Send a reset link to the given user.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Http\JsonResponse
-     */
-    public function sendResetLinkEmail(Request $request)
-    {
-        $this->validateEmail($request);
-
-        $response = $this->broker()->sendResetLink(
-            $this->credentials($request)
-        );
-
-        return $response == \Password::RESET_LINK_SENT
+        return $response == Password::RESET_LINK_SENT
             ? back()->with('status', trans($response))
             : back()->withInput($request->only('email'))
                 ->withErrors(['email' => trans($response)]);
